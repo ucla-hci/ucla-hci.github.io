cb6dde08f99a04867da64e9040a0c3dabbfdab22
Xiang 'Anthony' Chen <xac@xiangs-mbp.lan>
2019-04-13 21:48:22 -0700

    added team page

--- js/main.js
+++ js/main.js
@@ -4,24 +4,22 @@ var UCLAHCI = UCLAHCI || {};
 //	check mobile platform
 //
 window.mobilecheck = function () {
-	var check = false;
-	(function (a) {
-		if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true
-	})(navigator.userAgent || navigator.vendor || window.opera)
-	return check
+    var check = false;
+    (function (a) {
+        if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true
+    })(navigator.userAgent || navigator.vendor || window.opera)
+    return check
 };
 
 //
 //	check whether need to be responsive
 //
 UCLAHCI.checkResponsiveness = function () {
-	if ($(window).width() < 640 || window.mobilecheck() == true) {
-		$('td.topnav').attr('valign', 'top')
-	} else if ($(window).width() < 1150) {
-		// $('div.topnav').css('line-height', UCLAHCI.config.LOGOHEIGHT);
-	} else {
-		// $('div.topnav').css('line-height', UCLAHCI.config.LOGOHEIGHT);
-	}
+    if ($(window).width() < 640 || window.mobilecheck() == true) {
+        $('td.topnav').attr('valign', 'top')
+    } else if ($(window).width() < 1150) {
+    } else {
+    }
 }
 
 $(document).ready(function () {
@@ -29,12 +27,26 @@ $(document).ready(function () {
         UCLAHCI.config = result;
 
         UCLAHCI.checkResponsiveness();
-        
-        
-        // $('div.topnav').css('height', UCLAHCI.config.LOGOHEIGHT);
-        // $('img.logo').attr('src', UCLAHCI.config.LOGOURL)
-        // $('img.logo').css('height', UCLAHCI.config.LOGOHEIGHT);
 
+        YAML.load('data.yml', function (data) {
+            UCLAHCI.data = data;
+            // console.log(UCLAHCI.data);
+
+            UCLAHCI.updateUI();
+            UCLAHCI.updateUrl('team');
+
+            var page;
+            var idxSharp = location.href.indexOf('#');
+            if (idxSharp >= 0) page = location.href.substring(idxSharp + 1);
+            var idxDot = page.indexOf('.');
+
+            if (page != undefined) {
+                UCLAHCI.updatePage(page);
+                if (idxDot >= 0) {
+
+                }
+            }
+        });
     });
 });
 
@@ -45,4 +57,75 @@ function showHamMenu() {
     } else {
         x.className = "topnav";
     }
+}
+
+UCLAHCI.updatePage = function (page) {
+    $('#' + page).addClass('active');
+    // $('.content').html(page)
+    if (page == 'team') {
+        for (member of UCLAHCI.data.team) {
+            var div = $('<div/>');
+            div.addClass('team');
+            var tb = $('<table/>');
+            // span.addClass('team');
+            
+            var trImg = $('<tr/>');
+            var img = $('<img/>');
+            img.attr('src', 'assets/' + member.img);
+            img.addClass('team');
+            trImg.append(img);
+            tb.append(trImg);
+
+            var trName = $('<tr/>');
+            var divName = $('<div/>');
+            divName.addClass('info');
+            divName.html('<b>' + member.name + '</b>');
+            trName.append(divName);
+            tb.append(trName);
+
+            var trRole = $('<tr/>');
+            var divRole = $('<div/>');
+            divRole.addClass('info');
+            divRole.html(member.role + '<br>' + member.expertise);
+            trRole.append(divRole);
+            tb.append(trRole);
+            
+            div.append(tb);
+
+            $('.content').append(div);
+        }
+    } else if (page == 'projects') {
+
+    } else if (page == 'aboutus') {
+
+    }
+}
+
+UCLAHCI.updateUI = function () {
+    $('.topnav a').click(function (e) {
+        $('.topnav a').removeClass('active');
+        // $(e.target).addClass('active');
+        UCLAHCI.updatePage($(e.target).attr('id'));
+    });
+}
+
+//
+//  create a unique part of the url from the title
+//
+UCLAHCI.createUrl = function (title) {
+    if (title == undefined) return ''
+    return title.replace(/[&\/\\#,+()$~%.'":*;?<>{}]/g, '')
+        .replace(/ /g, '-').toLowerCase()
+}
+
+//
+//  update window url with project name (that's been clicked)
+//
+UCLAHCI.updateUrl = function (name) {
+    var idxSharp = location.href.indexOf('#')
+    var urlNew = location.href
+    if (idxSharp >= 0) urlNew = urlNew.substring(0, idxSharp)
+    // if(name != undefined) 
+    location.href = urlNew + '#' + UCLAHCI.createUrl(name)
+    // else location.href = urlNew
 }
